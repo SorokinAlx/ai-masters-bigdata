1b56050daf63b8228c85db3865220d2062977a1b
SorokinAlx <isashajeg@gmail.com>
2023-04-17 22:45:23 +0000

    SparkML hw

--- projects/4/model.py
+++ projects/4/model.py
@@ -1,14 +1,14 @@
 #!/opt/conda/envs/dsenv/bin/python
 
 from pyspark.ml.feature import StopWordsRemover, Tokenizer, VectorAssembler, HashingTF
-from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.regression import GBTRegressor
 from pyspark.ml import Pipeline
 
 tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
 stop_words = StopWordsRemover(inputCol="words",outputCol="words_without_stop", stopWords=StopWordsRemover.loadDefaultStopWords("english"))
 hasher = HashingTF(numFeatures=100, binary=True, inputCol="words_without_stop", outputCol="words_hashed")
-assembler = VectorAssembler(inputCols=['words_hashed'], outputCol="words_final")
-log_reg = LogisticRegression(featuresCol="words_final", labelCol="overall", maxIter=10, tol=1e-04)
+assembler = VectorAssembler(inputCols=['words_hashed', 'verified'], outputCol="words_final")
+log_reg = GBTRegressor(featuresCol="words_final", labelCol="overall", maxIter=12)
 
 pipeline = Pipeline(stages=[
     tokenizer,
