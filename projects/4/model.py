0aeaad9f1014db7a9ede25205501dcf41bc97a4c
SorokinAlx <isashajeg@gmail.com>
2023-04-18 14:40:37 +0000

    SparkML hw

--- projects/4/model.py
+++ projects/4/model.py
@@ -7,7 +7,7 @@ from pyspark.ml import Pipeline
 tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
 stop_words = StopWordsRemover(inputCol="words",outputCol="words_without_stop", stopWords=StopWordsRemover.loadDefaultStopWords("english"))
 hasher = HashingTF(numFeatures=110, binary=True, inputCol="words_without_stop", outputCol="words_hashed")
-assembler = VectorAssembler(inputCols=['words_hashed', outputCol="words_final")
+assembler = VectorAssembler(inputCols=['words_hashed'], outputCol="words_final")
 log_reg = GBTRegressor(featuresCol="words_final", labelCol="overall", maxIter=12)
 
 pipeline = Pipeline(stages=[
