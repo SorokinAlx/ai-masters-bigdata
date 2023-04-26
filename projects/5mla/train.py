3a7cc1bcbba71eb91a5708cb2502397e669f589c
SorokinAlx <isashajeg@gmail.com>
2023-04-26 01:34:29 +0000

    fix row size

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -9,11 +9,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
 from joblib import dump
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+
 
 #
 # Import model definition
 #
-from model import model, fields
 
 if __name__ == "__main__":
     #
@@ -33,6 +39,46 @@ if __name__ == "__main__":
     except:
         logging.critical("Need to pass both train path and l2-reg (C) parameter")
         sys.exit(1)
+
+    #
+    # Dataset fields
+    #
+    numeric_features = ["if"+str(i) for i in range(1,14)]
+    categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+
+    # without categorical features
+    fields = ["id", "label"] + numeric_features + categorical_features
+
+    #
+    # Model pipeline
+    #
+
+    class ColumnDropperTransformer():
+        def __init__(self, categorical_features):
+            self.columns=categorical_features
+        def transform(self,X,y=None):
+            return X.drop(self.columns,axis=1)
+        def fit(self, X, y=None):
+            return self 
+
+    # We create the preprocessing pipelines for both numeric and categorical data.
+    numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+    ])
+
+    preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+    ]
+    )
+    dropper = ColumnDropperTransformer(categorical_features)
+    # Now we have a full prediction pipeline.
+    model = Pipeline(steps=[
+    ('dropper', dropper),
+    ('preprocessor', preprocessor),
+    ('logregression', LogisticRegression())
+    ])
 
 
     logging.info(f"TRAIN_PATH {train_path}")
