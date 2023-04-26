883d389e2147202435f41dda396abf29e5723a29
SorokinAlx <isashajeg@gmail.com>
2023-04-26 00:08:50 +0000

    first_attempt

---
+++ projects/5mla/train.py
@@ -0,0 +1,66 @@
+#!/opt/conda/envs/dsenv/bin/python
+
+import os, sys
+import logging
+import mlflow
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+from joblib import dump
+
+#
+# Import model definition
+#
+from model import model, fields
+
+if __name__ == "__main__":
+    #
+    # Logging initialization
+    #
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("CURRENT_DIR {}".format(os.getcwd()))
+    logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
+    logging.info("ARGS {}".format(sys.argv[1:]))
+
+    #
+    # Read script arguments
+    #
+    try:
+        train_path = sys.argv[1] 
+        model_param1 = sys.argv[2]
+    except:
+        logging.critical("Need to pass both train path and l2-reg (C) parameter")
+        sys.exit(1)
+
+
+    logging.info(f"TRAIN_PATH {train_path}")
+    logging.info(f"C {model_param1}")
+    #
+    # Read dataset
+    #
+    #fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
+    #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+
+    read_table_opts = dict(sep="\t", names=fields, index_col=0)
+    df = pd.read_table(train_path, **read_table_opts)
+
+    #split train/test
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.iloc[:,1:14], df.iloc[:,0], test_size=0.33, random_state=42
+    )
+
+    #
+    # Train the model
+    #
+    with mlflow.start_run():
+        model.set_params(logregression__C=float(model_param1))
+        #mlflow.log_param("model_param1", float(model_param1))
+        mlflow.sklearn.log_model(model, artifact_path="tr")
+        model.fit(X_train, y_train)
+        y_pred = model.predict_proba(X_test)[:,1]
+        model_score = log_loss(y_test, y_pred)
+        mlflow.log_metric("log_loss", model_score)
+
+
