fe71f9185c64b7c2787a37c8c588eb86c53b9c24
SorokinAlx <isashajeg@gmail.com>
2023-05-02 16:04:53 +0000

    spark-submit

--- projects/6/train.py
+++ projects/6/train.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from joblib import dump
 
-train = pd.read_json(sys.argv[2], lines=True)
+train = pd.read_json(sys.argv[2], lines=False)
 train = train[["label","words_final"]]
 for i in range(100):
     train[f"words_{i}"]=train["words_final"].apply(lambda x: x[i])
