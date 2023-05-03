97e661ad0a0ccb95dffea5a0e653705e11f5b159
SorokinAlx <isashajeg@gmail.com>
2023-05-03 12:46:46 +0000

    spark-submit

--- projects/6/train.py
+++ projects/6/train.py
@@ -7,8 +7,8 @@ train = pd.read_json(sys.argv[2], lines=True)
 print(train.head(5))
 train = train[["label","words_final"]]
 for i in range(100):
-    train[f"words_{i}"]=train["words_final"].apply(lambda x: x[i])
-
+    train[f"words_{i}"]=train["words_final"].apply(lambda x: x["indices"][i])
+print(train.head(5))
 log_reg = LogisticRegression()
 model = log_reg.fit(train.iloc[:,2:], train.iloc[:,0])
 dump(model, sys.argv[4])
